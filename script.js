354b3a0856747babd466fb79eaae1d5fc1b5117a
amiraghabiglou <a.aghabiglou@hw.ac.uk>
2024-02-08 18:58:26 +0000

    The library upgraded to BASPLib

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// Function to redirect after 30 seconds
+function redirectToNewPage() {
+    window.location.href = "https://basp-group.github.io/BASPLib/index.html";
+}
+
+// Function to start the countdown and redirect
+function startCountdown() {
+    var countdown = 30; // Countdown time in seconds
+    var countdownInterval = setInterval(function() {
+        document.getElementById('countdown').innerText = countdown + ' sec.';
+        countdown--;
+        if (countdown < 0) {
+            clearInterval(countdownInterval);
+            redirectToNewPage();
+        }
+    }, 1000); // Update countdown every second
+}
+
+// Call startCountdown when the page loads
+window.onload = startCountdown;
